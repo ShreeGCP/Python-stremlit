3af4d349a4ed4d17db6343be3b2f68aa31f53df7
ShreeGCP <devadigashridhara@gmail.com>
2024-08-28 11:01:47 +0530

    day 22 - adding title for two columns

--- portfolo.py
+++ portfolo.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas
+
 st.set_page_config(layout="wide")
 col1,col2 = st.columns(2)
 
@@ -18,3 +20,18 @@ Never Ever Giveup....Good Things always takes time"""
 content1 = """Below you can find some of the apps.I am Python Developer 
 Feel Free to contanct me any work"""
 st.write(content1)
+
+df = pandas.read_csv("data.csv",sep=";")
+col3,col4 = st.columns(2)
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
+
+
+
+
+
