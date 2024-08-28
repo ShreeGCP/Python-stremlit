3e5c0813a5d18552bdf76127ca43e890f8832f80
ShreeGCP <devadigashridhara@gmail.com>
2024-08-28 12:12:31 +0530

    Design completed

--- portfolo.py
+++ portfolo.py
@@ -22,14 +22,21 @@ Feel Free to contanct me any work"""
 st.write(content1)
 
 df = pandas.read_csv("data.csv",sep=";")
-col3,col4 = st.columns(2)
+#col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 with col4:
     for index,row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+ row["image"])
+        st.write(f"[source code]({row['url']})")
+
 
 
 
